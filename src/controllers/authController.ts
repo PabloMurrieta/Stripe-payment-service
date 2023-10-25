490ac8d52a95fd524dfa4d2f45d8202c62368e64
Juan Pablo Murrieta <juanpablomurrieta3@gmail.com>
2023-10-25 16:07:42 -0700

    enpoint 'register' forma base hecho

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from "express";
+import User from "../models/user";
 
 
 export const register = async (req: Request, res: Response) =>{
 
-    res.json({msg:'register'})
-}
+    const {username, password, email} = req.body;
+    try {
+
+        const user = new User({username, password, email});
+        await user.save();
+        res.status(200).json({msg:'User registrado exitosamente', user});
+        
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+};
 
 export const login = async (req: Request, res: Response) =>{
 
     res.json({msg:'login'})
 
-}
+};
 
 
 
